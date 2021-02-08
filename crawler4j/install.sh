79dda10e71007ffa8225d6f69ca23d40aa0c4a3b
brett-matson <brett.matson@gmail.com>
2021-02-08 17:16:08 +1100

    Added working pom and build scripts

---
+++ crawler4j/install.sh
@@ -0,0 +1,7 @@
+mvn install:install-file \
+   -Dfile=target/crawler4j-4.5.0.jar \
+   -DgroupId=edu.uci.ics \
+   -DartifactId=crawler4j \
+   -Dversion=4.5.0 \
+   -Dpackaging=jar \
+   -DgeneratePom=true
